b9112f9df87c87573394c5fe537e637bad2e93b9
daniel <jhz8s2@naver.com>
2022-10-01 19:31:21 +0900

    coding test

---
+++ codingTest/src/programmers/proLevel2_멀리뛰기.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class proLevel2_¸Ö¸®¶Ù±â {
+
+	public static void main(String[] args) {
+		int n = 4;
+		
+		System.out.println(solution(n));
+	}
+	
+	public static long solution(int n) {
+        long[] fibo = new long[2001];
+        
+        fibo[1] = 1;
+        fibo[2] = 2;
+        
+        for (int i = 3; i < fibo.length; i++) {
+			fibo[i] = (fibo[i - 2] + fibo[i - 1]) % 1234567;
+		}
+        
+        return fibo[n];
+    }
+
+}
